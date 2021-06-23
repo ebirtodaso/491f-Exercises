bef716dd7ab4d853949b1349494487912db552e2
ebirtodaso <edwardkb@hawaii.edu>
2021-06-22 14:39:44 -1000

    Added filter and ordering to blog

--- Lists Application with Posts/blog/urls.py
+++ Lists Application with Posts/blog/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/new/', views.post_new, name='new'),
     path('post/<int:pk>/edit/', views.post_edit, name='edit'),
+    path('post/<int:pk>/edit/delete/', views.post_delete, name='delete'),
+    path('order_by_author/', views.post_list_author, name='post_by_author'),
+    path('filter_by_published/', views.post_list_published, name='filter_by_published'),
 ]
--- Lists Application with Posts/blog/views.py
+++ Lists Application with Posts/blog/views.py
@@ -7,7 +7,18 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_list_author(request):
+    posts = Post.objects.order_by('author')
+    
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_list_published(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    
+    return render(request, 'blog/post_list.html', {'posts':posts})        
 
 def post_new(request):
 
@@ -27,19 +38,27 @@ def post_new(request):
   return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
+  
+  post = get_object_or_404(Post, pk=pk)
+  nickel = 3
+  if request.method == "POST":
+      form = PostForm(request.POST, instance=post)
+      if form.is_valid():
+          post = form.save(commit=False)
+          post.author = request.user
+          post.published_date = timezone.now()
+          post.save()
+          return redirect('blog:post_list')
+  else:
+      form = PostForm(instance=post)
+
+  return render(request, 'blog/post_edit.html', {'form': form,'post': post})
+
+def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('blog:post_list')
-    else:
-        form = PostForm(instance=post)
-
-    return render(request, 'blog/post_edit.html', {'form': form})
+    post.delete()
+    
+    return redirect('blog:post_list')
 
 
